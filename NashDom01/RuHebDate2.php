f3a66eb834c79ef1a5d182dff7cb65e613d5405a
Guy (Yahalom) <guy@yahaloms.com>
2021-11-22 09:47:54 +0300

    thephp

---
+++ NashDom01/RuHebDate2.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html>
+
+
+
+
+    <!-- <?php
+    // The file test.xml contains an XML document with a root element
+    // and at least an element /[root]/title.
+    
+    if (file_exists('https://www.hebcal.com/etc/hdate-en.xml')) {
+        $xml = simplexml_load_file('https://www.hebcal.com/etc/hdate-en.xml');
+     
+        print_r($xml);
+    } else {
+        exit('Failed to open the file.');
+    }
+    ?> -->
+
+    <!-- <table width="100%">
+        <tr>
+            <td width="45%">
+                <div id="heDateHeb" style="font-size: 50px; color: white; font-weight: bold; text-align: left;">
+                    "https://www.hebcal.com/etc/hdate-en.xml"
+                </div>
+
+            </td>
+            <td width="5%">
+                 <div id="heDate" style="font-size: 0px; color: white; font-weight: bold; text-align: left;">
+                    <script type=" text/javascript " charset="utf-8 " src="https://www.hebcal.com/etc/hdate-en.js "></script>
+                </div> 
+            </td>
+
+            <td width="50%">
+                <div id="ruDate" style="font-size: 50px;text-align: left; color: white; font-weight: bold;">
+                to be Ru Date
+                </div>
+            </td>
+        </tr>
+    </table>
+    <br>
+    <php>
+        $doc = new DomDocument;
+        $doc->validateOnParse = true;
+        $doc->Load('https://www.hebcal.com/etc/hdate-en.xml');
+    </php>
+
+    <script>
+        $(document).ready(function() {
+            console.log("Updating RuHebDate");
+            var hebDate = document.getElementById("heDate").innerText;
+            var hebArray = hebDate.split(" ");
+            var currDay = hebArray[0].substr(0, hebArray[0].length - 2);
+            var engMonth = hebArray[2].substr(0, hebArray[2].length - 1);
+            var ruMonth = "";
+            switch (engMonth) {
+                case "Tishrei":
+                    ruMonth = "Тишрей";
+                    break;
+                case "Cheshvan":
+                    ruMonth = "Хешван";
+                    break;
+                case "Kislev":
+                    ruMonth = "Кислев";
+                    break;
+                case "Tevet":
+                    ruMonth = "Тевет";
+                    break;
+                case "Sh'vat":
+                    ruMonth = "Шват";
+                    break;
+                case "Adar":
+                    ruMonth = "Адар";
+                    break;
+                case "Adar I":
+                    ruMonth = "Адар 1";
+                    break;
+                case "Adar II":
+                    ruMonth = "Адар 2";
+                    break;
+                case "Nisan":
+                    ruMonth = "Нисан";
+                    break;
+                case "Iyyar":
+                    ruMonth = "Ияр";
+                    break;
+                case "Sivan":
+                    ruMonth = "Сиван";
+                    break;
+                case "Tamuz":
+                    ruMonth = "Тамуз";
+                    break;
+                case "Av":
+                    ruMonth = "Ав";
+                    break;
+                case "Elul":
+                    ruMonth = "Элул";
+                    break;
+                default:
+                    ruMonth = engMonth;
+            }
+            currYear = hebArray[3];
+            $("#ruDate").html(currDay + " " + ruMonth + ", " + currYear)
+        });
+    </script> -->
